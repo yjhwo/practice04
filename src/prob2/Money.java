6056919d7f11c302defe337481bae62ed5b796b5
kickscar <kickscar@gmail.com>
2016-03-09 05:42:11 +0900

    1st commit

---
+++ src/prob2/Money.java
@@ -0,0 +1,22 @@
+package prob2;
+
+public class Money {
+	private int amount;
+    public Money(int amount) {
+        this.amount = amount;
+    }
+    public Money add( Money money ) {
+        //더하기
+    }
+    public Money minus( Money money ) {
+        //빼기
+    }
+    public Money multiply( Money money ) {
+        //곱하기
+    }
+    public Money devide( Money money ) {
+        //나누기
+    }
+    
+
+}
